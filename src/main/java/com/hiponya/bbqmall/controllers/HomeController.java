2bb0d8018746de28d06d1e8be134a26ddf591bf7
Administrator <kwack0518@naver.com>
2022-12-08 15:38:24 +0900

    Setting

--- src/main/java/com/hiponya/bbqmall/controllers/HomeController.java
+++ src/main/java/com/hiponya/bbqmall/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.hiponya.bbqmall.controllers;
 
+import com.hiponya.bbqmall.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.http.MediaType;
@@ -12,9 +14,15 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/")
 public class HomeController {
 
+    private final MemberService memberService;
+
+    @Autowired
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
     @GetMapping(value = "/" ,produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getHome(){
-        ModelAndView modelAndView = new ModelAndView("home/index");
+        ModelAndView modelAndView = new ModelAndView("member/login");
 
         return modelAndView;
     }
