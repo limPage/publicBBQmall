5577f08a104f7e716c2d822263a54dcdb70d6028
Administrator <kwack0518@naver.com>
2022-12-08 12:46:54 +0900

    Add MemberService

---
+++ src/main/java/com/hiponya/bbqmall/services/MemberService.java
@@ -0,0 +1,7 @@
+package com.hiponya.bbqmall.services;
+
+import org.springframework.stereotype.Service;
+
+@Service(value="com.hiponya.bbqmall.services.MemberService")
+public class MemberService {
+}
