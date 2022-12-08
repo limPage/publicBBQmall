323d7454604946e3909657adf0ab6ad33b4ab4fa
limcheolhong <jjirong2e@naver.com>
2022-12-08 14:10:59 +0900

    start

---
+++ src/main/java/dev/limpage/bbqmall/BbqmallApplication.java
@@ -0,0 +1,13 @@
+package dev.limpage.bbqmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbqmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbqmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/limpage/bbqmall/BbqmallApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.limpage.bbqmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbqmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
