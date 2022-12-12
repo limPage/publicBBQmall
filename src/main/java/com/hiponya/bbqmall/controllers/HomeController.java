331a32917d6cda27bd4d13b877c654fdea4b62fa
hiponyam <80513253+hiponyam@users.noreply.github.com>
2022-12-12 17:51:06 +0900

    detailmenu-ing

--- src/main/java/com/hiponya/bbqmall/controllers/HomeController.java
+++ src/main/java/com/hiponya/bbqmall/controllers/HomeController.java
@@ -1,11 +1,8 @@
 package com.hiponya.bbqmall.controllers;
 
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -38,6 +35,13 @@ public class HomeController {
     @GetMapping(value = "category", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getCategory() {
         ModelAndView modelAndView = new ModelAndView("home/category");
+
+        return modelAndView;
+    }
+
+    @GetMapping(value = "detailmenu", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getDetailMenu() {
+        ModelAndView modelAndView = new ModelAndView("home/detailmenu");
 
         return modelAndView;
     }
