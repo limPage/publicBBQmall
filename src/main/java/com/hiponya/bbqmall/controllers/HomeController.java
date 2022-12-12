e21d682eace98d0cdc6f1d3abbf693b6c1845cae
hiponyam <80513253+hiponyam@users.noreply.github.com>
2022-12-12 09:53:03 +0900

    Register-ing

--- src/main/java/com/hiponya/bbqmall/controllers/HomeController.java
+++ src/main/java/com/hiponya/bbqmall/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.hiponya.bbqmall.controllers;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,6 +24,20 @@ public class HomeController {
     @GetMapping(value = "register", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getRegister() {
         ModelAndView modelAndView = new ModelAndView("member/register");
+
+        return modelAndView;
+    }
+
+    @GetMapping(value = "catalog", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getCatalog() {
+        ModelAndView modelAndView = new ModelAndView("member/catalog");
+
+        return modelAndView;
+    }
+
+    @GetMapping(value = "category", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getCategory() {
+        ModelAndView modelAndView = new ModelAndView("home/category");
 
         return modelAndView;
     }
