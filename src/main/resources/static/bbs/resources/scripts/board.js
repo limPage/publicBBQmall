497545f30a9ab51a8fe62f3729591b085ad8810d
LIM <jjirong2e@naver.com>
2022-12-27 01:24:09 +0900

    공지 리스트 불러오는 서비스 매퍼를 하나로 통합

--- src/main/resources/static/bbs/resources/scripts/board.js
+++ src/main/resources/static/bbs/resources/scripts/board.js
@@ -1,45 +1,75 @@
-function save ()  {
+if (form['isNoticeBoard'].value==="notice"){
+//공지사항 탭일 경우
 
-    window.localStorage.savedNoticeBoardId = form['bid'].value;
+    function save ()  {
+
+        window.localStorage.savedNoticeBoardId = form['bid'].value;
+
+    }
+
+    const load = () => {
+        form['bid'].value = window.localStorage.savedNoticeBoardId ?? 'all';
+    };
+    window.addEventListener('load', () =>{
+        load();
+        delete window.localStorage.savedNoticeBoardId;
+        // setInterval(save,1000);
+
+    })
+
+    //     window.addEventListener('load', function () {
+    //
+    //
+    //    delete window.localStorage.savedNoticeBoardId
+    //         form['bid'].value='all'
+    //
+    //
+    // })
+
+
+
+
+
+    form['bid'].oninput=()=>{
+        save ();
+
+        document.form.submit();
+    }
+    form.onsubmit=()=>{
+
+
+        save ();
+    }
+    let page= window.document.querySelectorAll('.page')
+    for( let i = 0; i < page.length; i++ ) {
+        let item = page.item(i);
+
+        item.addEventListener('click', save);
+    }
+
 
 }
 
-const load = () => {
-    form['bid'].value = window.localStorage.savedNoticeBoardId ?? 'all';
-};
-window.addEventListener('load', () =>{
-    load();
-    delete window.localStorage.savedNoticeBoardId;
-    // setInterval(save,1000);
-
-})
-
-//     window.addEventListener('load', function () {
-//
-//
-//    delete window.localStorage.savedNoticeBoardId
-//         form['bid'].value='all'
-//
-//
-// })
+else if (form['isNoticeBoard'].value === "qna"){
 
 
+   const questionOpen= window.document.querySelectorAll('.questionOpen');
+   const antherLi=  window.document.querySelectorAll('.anther');
 
+    for(let i=0 ;i< questionOpen.length; i++){
 
+        const questionText= questionOpen.item(i);
+        const antherText= antherLi.item(i);
 
-form['bid'].oninput=()=>{
-    save ();
+        questionText.addEventListener('click', () => {
 
-    document.form.submit();
-}
-form.onsubmit=()=>{
+             if  (antherText.classList.contains('visible')){
+                antherText.classList.remove('visible');
+            }else {
+                antherText.classList.add('visible');
+                 }
+            }
+    )}
 
+ }
 
-    save ();
-}
-let page= window.document.querySelectorAll('.page')
-for( let i = 0; i < page.length; i++ ) {
-    let item = page.item(i);
-
-    item.addEventListener('click', save);
-}
