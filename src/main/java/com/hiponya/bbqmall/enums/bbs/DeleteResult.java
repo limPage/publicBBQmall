0210e6d2eb7c8142fb2ef8b6671bf40ebb3aad8f
LIM <jjirong2e@naver.com>
2023-01-02 00:52:47 +0900

    대량구매 완

---
+++ src/main/java/com/hiponya/bbqmall/enums/bbs/DeleteResult.java
@@ -0,0 +1,11 @@
+package com.hiponya.bbqmall.enums.bbs;
+
+
+import com.hiponya.bbqmall.interfaces.IResult;
+
+public enum DeleteResult implements IResult {
+    NO_SUCH_ARTICLE,
+    NOT_ALLOWED,
+    NOT_SIGNED
+
+}
